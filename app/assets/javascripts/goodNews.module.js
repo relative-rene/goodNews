bc1d0f06937952a17949cfe26842dc1aa2a77957
relative-rene <rene.arellano415@gmail.com>
2016-10-21 15:15:25 -0700

    [chore] front end routing

--- app/assets/javascripts/goodNews.module.js
+++ app/assets/javascripts/goodNews.module.js
@@ -1,1 +1,20 @@
-angular.module('goodNews', ['ngRoute', 'templates']);
+angular.module('goodNews', ['ngRoute', 'templates']).config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'fights.template.html',
+        controller: 'FightsController',
+        controllerAs: 'fightsCtrl'
+    }).when('/fights/:fightId', {
+        templateUrl: 'fight.template.html', // watch the 's'
+        controller: 'FightController',
+        controllerAs: 'fightCtrl'
+    }).when('/fights', { //note same as above, JIC
+        templateUrl: 'fights.template.html',
+        controller: 'FightsController',
+        controllerAs: 'fightsCtrl'
+    }).otherwise({redirectTo: '/'});
+
+    $locationProvider.html5Mode({enabled: true, requireBase: false});
+}
